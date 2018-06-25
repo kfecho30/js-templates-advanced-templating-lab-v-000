4952783d125f29be880eb68dd02f9c9afb6c899f
Kelsey Fecho <kfecho30@gmail.com>
2018-06-25 19:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+function initForm() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+}
+
 function init() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 
-  function initForm() {
-    var formTemplate = document.getElementById("recipe-form-template").innerHTML
-    var template = Handlebars.compile(formTemplate)
-  }
+  initForm();
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
