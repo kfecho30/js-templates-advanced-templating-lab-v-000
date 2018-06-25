69ab9fd9aed09a99f2b32157fcf428416f9c79d0
Kelsey Fecho <kfecho30@gmail.com>
2018-06-25 19:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function initForm() {
 }
 
 function createRecipe(){
+  var recipe = getRecipeVals()
   var recipeTemplate = document.getElementById("recipe-template").innerHTML
   var template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
