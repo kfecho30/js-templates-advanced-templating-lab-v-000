3f4def09a24b72170bc1f10de296a8f455eb7550
Kelsey Fecho <kfecho30@gmail.com>
2018-06-25 19:04:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function initForm() {
 }
 
 function init() {
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 
